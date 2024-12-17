e6a81d78b3babfeaa4e899d567cdbd2140c20686
Mr. Charles <94595457+CharlesPlay3zGam3s@users.noreply.github.com>
2024-12-17 21:48:50 +0000

    Calc Day 1

--- classwork.cpp
+++ classwork.cpp
@@ -7,5 +7,41 @@
 int main()
 {
 
+
+
+  int user_input = 0;
+  int num1;
+  int num2;
+  int numerator;
+  int denominator;
+  while (user_input != 99)
+  {
+    std::cout << "Welcome to C++ Calculator!  Choose an option below!\n"
+              << "0. Roll a dice\n"        
+              << "1. Add\n"
+              << "2. Subtract\n"
+              << "3. Multiply\n"
+              << "4. Divide\n"
+              << "5. Square root\n"
+              << "99. Quit\n\n> ";
+
+    std::cin >> user_input;
+    
+    // implement calculator below
+    if (user_input == 0)
+    {
+      int random_number = (std::rand() % 6) + 1;  // random numbers between 1 and 6
+      std::cout << "You rolled a " << random_number << std::endl;
+    }
+    else
+    {
+      if (user_input == 1)
+      {
+        std::cout << "Enter two numbers\n";
+        std::cin >> num1 >> num2;
+        std::cout << "Their sum is " << (num1 + num2) << std::endl;
+      }
+    }
+  }
   return 0;
 }
